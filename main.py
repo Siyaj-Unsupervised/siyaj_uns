1c29b3758d85ae31189c5a7028f6e298e47c1045
Yara-A-R <yarayear2056@gmail.com>
2026-01-25 22:17:38 +0300

    Built and tested ensemble of models for Siyaj system

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from siyaj-uns!")
+from scripts.ensemble import siyaj_predict
+import pandas as pd
 
+def main():
+    print("🚀 جاري تشغيل نظام سياج الموحد...")
+
+    test_data = pd.read_csv(
+        'data/clean/Wednesday-WorkingHours.pcap_ISCX_cleaned.csv'
+    ).head(20)
+
+    X = test_data.drop(['Label'], axis=1) if 'Label' in test_data.columns else test_data
+
+    predictions = siyaj_predict(X)
+
+    test_data['Siyaj_Decision'] = predictions
+    print(test_data[['Siyaj_Decision']])
 
 if __name__ == "__main__":
     main()
